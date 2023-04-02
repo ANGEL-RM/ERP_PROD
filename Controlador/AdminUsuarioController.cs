6aec75df1cc7f96cb261b283ddde77e78164c7fd
Angel RM <angel_ruiz.m@hotmail.com>
2023-04-01 20:23:26 -0600

    Se agregó la clase Negocio y se modifico el Entity

--- Controlador/AdminUsuarioController.cs
+++ Controlador/AdminUsuarioController.cs
@@ -6,14 +6,35 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelo.ACCESOBD;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Controlador
 {
     public class AdminUsuarioController
     {
-        public async Task<List<Tbl_Adm_Usuario>> ObtenerUsuarios()
+        private AccesoBD db;
+
+        public AdminUsuarioController()
         {
-            List<Tbl_Adm_Usuario> Usuarios = new List<Tbl_Adm_Usuario>();
+        }
+
+        public AdminUsuarioController(AccesoBD db)
+        {
+            this.db = db;
+        }
+        public List<Tbl_Adm_Usuario> ObtenerUsuarios()
+        {
+            
+
+
+
+
+
+
+
+                List<Tbl_Adm_Usuario> Usuarios = new List<Tbl_Adm_Usuario>();
             using (SqlConnection conn = new SqlConnection(ConexionBD.cadena_conexion))
             {
                 try
@@ -55,73 +76,21 @@ namespace Controlador
 
 
 
-        public Respuesta<int> ValidarUsuarioLogin(Tbl_Adm_Usuario usuario)
+        public async Task<Respuesta<int>> ValidarUsuarioLogin(Tbl_Adm_Usuario usuario)
         {
+
             var respuesta = new Respuesta<int> { Estado = EstadosDeRespuesta.Correcto };
 
-            List<Tbl_Adm_Usuario> Usuarios = new List<Tbl_Adm_Usuario>();
-            using (SqlConnection conn = new SqlConnection(ConexionBD.cadena_conexion))
+
+            if (respuesta.Estado == EstadosDeRespuesta.Correcto)
             {
-                try
-                {
-                    string query = "select * from Tbl_Adm_Usuario where Nombre= '" + usuario.Nombre + "' and Password= '" + usuario.Password + "'";
-                    SqlCommand sqlCommand = new SqlCommand(query, conn);
-                    sqlCommand.CommandType = CommandType.Text;
-                    conn.Open();
-                    using (SqlDataReader dr = sqlCommand.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            Usuarios.Add(new Tbl_Adm_Usuario()
-                            {
-                                UsuarioId = (int)dr["UsuarioId"],
-                                Password = (string)dr["Password"],
-                                Nombre = (string)dr["Nombre"],
-                                ApellidoP = (string)dr["ApellidoP"],
-                                ApellidoM = (string)dr["ApellidoM"],
-                                Estado = (bool)dr["Estado"],
-                                FechaIngreso = (DateTime)dr["FechaIngreso"],
-                                FechaAltaSys = (DateTime)dr["FechaAltaSys"],
-                                RolId = (int)dr["RolId"],
-                                EsCambiarPassword = (bool)dr["EsCambiarPassword"],
-                                RecoverPassword = (string)dr["RecoverPassword"]
-                            });
-                        }
-                    }
 
-                    if (Usuarios.Count != 0)
-                    {
-                            if (Usuarios.First().Password != null)
-                            {
-                                if (Usuarios.First().Password != usuario.Password)
-                                {
-                                    respuesta.Estado = EstadosDeRespuesta.NoProceso;
-                                    respuesta.Mensaje = "Contraseña Incorrecta";
-                                }
-                                else
-                                {
-                                    respuesta.Datos = 2;
-                                }
-                            }
-                            else
-                            {
-                                respuesta.Estado = EstadosDeRespuesta.NoProceso;
-                                respuesta.Mensaje = "Ingresa Contraseña";
-                            }
-                        
-                    }
-                    else
-                    {
-                        respuesta.Datos = 1;
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    respuesta.Estado = EstadosDeRespuesta.Error;
-                    respuesta.Mensaje = $"ocurrio un error al obtener los usuarios: {ex.Message}";
-                }
             }
+        
+            //Tbl_Adm_Usuario usuarioBd = await db.Tbl_Adm_Usuarios.FirstOrDefaultAsync(f => f.Nombre.Equals(usuario.Nombre));
+
+            //List<Tbl_Adm_Usuario> Usuarios = new List<Tbl_Adm_Usuario>();
+            
             return respuesta;
         }
     }
--- Modelo/ACCESOBD/AccesoBD.cs
+++ Modelo/ACCESOBD/AccesoBD.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -7,8 +11,25 @@ using System.Threading.Tasks;
 
 namespace Modelo.ACCESOBD
 {
-    public class AccesoBD
+    public class AccesoBD: DbContext
     {
+        public AccesoBD(DbContextOptions<AccesoBD> options) : base(options){}
 
+        public AccesoBD()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            //var configuracion = config.Build();
+            //var valor = configuracion.GetValue<string>("Connection");
+            //options.UseSqlServer(valor);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+        public virtual DbSet<Tbl_Adm_Usuario> Tbl_Adm_Usuarios { get; set; }
+    
     }
 }
